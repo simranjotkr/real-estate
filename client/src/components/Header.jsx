2de84269817e80db6ebd74b20b915d95d1d47645
SimranjotKaur <taneja.simranjot@gmail.com>
2023-10-20 16:12:55 -0400

    created and run server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,8 +8,8 @@ export default function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex-wrap">
-            <span className="text-slate-500 ">Simran</span>
-            <span className="text-slate-700">Estate</span>
+            <span className="text-slate-500 ">Re</span>
+            <span className="text-slate-700">Boot</span>
           </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
